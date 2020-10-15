209557ad6e555e57087ff10c57685c00b315feaf
CagdasAyyildiz <cagdasayyildiz.ayyildiz@gmail.com>
2020-10-15 13:48:28 +0300

    Added new example

--- Template Literals/template_literals.js
+++ Template Literals/template_literals.js
@@ -1,9 +1,18 @@
-function templateLiteral(strings, ...rest) {
+//Both give the same result
+
+function templateLiteral1(strings, ...rest) {
     return `${strings[0]}${rest[0]}${strings[1]}${rest[1]}`
+}
+
+function templateLiteral2(strings, value1, value2) {
+    return `${strings[0]}${value1}${strings[1]}${value2}`
 }
 
 let template = 'template'
 let fine = 'fine'
 
-let myString = templateLiteral`This is a ${template} and it works ${fine}`
+let myString = templateLiteral1`This is a ${template} and it works ${fine}`
+console.log(myString)
+
+myString = templateLiteral2`This is a ${template} and it works ${fine}`
 console.log(myString)
